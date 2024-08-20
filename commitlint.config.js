974648f91508feaf3d7185126e470c35253624b8
Alysson Lopes <alysson.als@gmail.com>
2024-08-20 00:18:21 -0300

    chore: update eslint and commitlint configurations

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,24 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
+export default {
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "type-enum": [
+    'type-enum': [
       2,
-      "always",
+      'always',
       [
-        "build",
-        "chore",
-        "ci",
-        "docs",
-        "feat",
-        "fix",
-        "perf",
-        "refactor",
-        "revert",
-        "style",
-        "test",
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
       ],
     ],
-    "subject-full-stop": [2, "never", "."],
-    "header-max-length": [2, "always", 72],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
   },
 };
---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import nextPlugin from '@next/eslint-plugin-next';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.url,
+});
+
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      next: nextPlugin,
+      '@typescript-eslint': tsPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+  ...compat.extends('next/core-web-vitals'),
+  {
+    rules: {
+      '@next/next/no-duplicate-head': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+];
