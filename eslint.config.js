4de9b9e79659fb3669eeb0e97c6b8fb9a224829e
Alysson Lopes <alysson.als@gmail.com>
2024-08-20 21:10:57 -0300

    chore: add hooks lint

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import { FlatCompat } from '@eslint/eslintrc';
 import nextPlugin from '@next/eslint-plugin-next';
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.url,
@@ -16,6 +17,7 @@ export default [
     plugins: {
       next: nextPlugin,
       '@typescript-eslint': tsPlugin,
+      'react-hooks': reactHooksPlugin,
     },
     languageOptions: {
       parser: tsParser,
@@ -27,6 +29,7 @@ export default [
       ...tsPlugin.configs.recommended.rules,
       ...nextPlugin.configs.recommended.rules,
       ...nextPlugin.configs['core-web-vitals'].rules,
+      ...reactHooksPlugin.configs.recommended.rules,
     },
   },
   ...compat.extends('next/core-web-vitals'),
